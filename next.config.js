e40bd975b4f43f97a61780fef451985b8cb82045
LinkaFilip <filip.linka@email.cz>
2025-05-24 21:49:03 +0000

    Remove Next.js and switch to static HTML

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // tohle je novinka místo next export
-  // další nastavení...
-}
-
-module.exports = nextConfig;

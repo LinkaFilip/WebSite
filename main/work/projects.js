cb340a4da00bdce99e6a2ecda3204674146d8ddb
LinkaFilip <filip.linka@email.cz>
2025-06-29 16:16:03 +0000

    changes

--- main/work/projects.js
+++ main/work/projects.js
@@ -5,6 +5,19 @@ const projects = [
     link: "/wrongly_developed_dreams/"
   },
   {
+    title: "Ponor",
+    image: "/images/ponor_showcase.webp",
+  },
+  {
+    title: "Mandator",
+    image: "/images/mandator_showcase.webp",
+    link: "/mandator/"
+  },
+  {
+    title: "Arlington",
+    image: "/images/arlington_showcase.webp",
+  },
+  {
     title: "The last one before bedtime",
     image: "/images/the_last_one_before_bedtime.webp",
     link: "/the_last_one_before_bedtime/"

3c18f3262c4a51adc9bb0da590fe92451492c42d
LinkaFilip <filip.linka@email.cz>
2025-06-29 10:25:21 +0000

    changes

---
+++ main/work/projects.js
@@ -0,0 +1,27 @@
+const projects = [
+  {
+    title: "Random",
+    image: "/images/wrongly_developed_dreams.webp",
+    link: "/wrongly_developed_dreams/"
+  },
+  {
+    title: "Wrongly developed dreams",
+    image: "/images/wrongly_developed_dreams.webp",
+    link: "/wrongly_developed_dreams/"
+  },
+  {
+    title: "White puppies",
+    image: "/images/white_puppiesquality=2.webp",
+    link: "/white_puppies/"
+  },
+  {
+    title: "The last one before bedtime",
+    image: "/images/the_last_one_before_bedtime.webp",
+    link: "/the_last_one_before_bedtime/"
+  },
+  {
+    title: "Makulatura",
+    image: "/images/makulaturaquality=2.webp",
+    link: "/makulatura/"
+  }
+];

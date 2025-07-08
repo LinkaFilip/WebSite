b2195726986d9bb00a1c00f1d646ad4008b1b4e8
LinkaFilip <filip.linka@email.cz>
2025-07-08 20:56:41 +0000

    changes

--- main/work/projects.js
+++ main/work/projects.js
@@ -7,29 +7,35 @@ const projects = [
   },
   {
     title: "Ponor",
-    imageHigh: "/images/Ponor-small.webp",
+    imageLow: "/images/Ponor-small.webp",
+    imageHigh: "/images/Ponor-small.webp"
   },
   {
     title: "Mandator",
     imageHigh: "/images/Mandator-small.webp",
+    imageLow: "/images/Mandator-small.webp",
     link: "/mandator/"
   },
   {
     title: "Arlington",
+    imageLow: "/images/Arlington-small.webp",
     imageHigh: "/images/Arlington-small.webp",
   },
   {
     title: "The last one before bedtime",
     imageHigh: "/images/the_last_one_before_bedtime.webp",
+    imageLow: "/images/the_last_one_before_bedtime.webp",
     link: "/the_last_one_before_bedtime/"
   },
   {
     title: "Makulatura",
     imageHigh: "/images/makulaturaquality=2.webp",
+    imageLow: "/images/makulaturaquality=2.webp",
     link: "/makulatura/"
   },
   {
     title: "White puppies",
+    imageLow: "/images/white_puppiesquality=2.webp",
     imageHigh: "/images/white_puppiesquality=2.webp",
     link: "/white_puppies/"
   }

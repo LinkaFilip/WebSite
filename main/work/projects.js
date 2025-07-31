9fb7359ea0d63b2228adf6b4cc397c64440b212c
LinkaFilip <filip.linka@email.cz>
2025-07-31 22:09:53 +0200

    change

--- main/work/projects.js
+++ main/work/projects.js
@@ -1,35 +1,54 @@
 const projects = [
   {
     title: "Wrongly developed dreams or 4 harbingers of strangled sadness or when angels deserve only death",
-    image: "/images/wrongly_developed_dreams.webp",
+    imageLow: "/images/wrongly_developed_dreamsquality=2.webp", 
+    imageHigh: "/images/wrongly_developed_dreams.webp",
     link: "/wrongly_developed_dreams/"
   },
   {
     title: "Ponor",
-    image: "/images/Ponor-small.webp",
+    imageLow: "/images/Ponor-small.webp",
+    imageHigh: "/images/Ponor-small.webp"
   },
   {
     title: "Mandator",
-    image: "/images/Mandator-small.webp",
+    imageHigh: "/images/Mandator-small.webp",
+    imageLow: "/images/Mandator-small.webp",
     link: "/mandator/"
   },
   {
+    title: "The sun in the brightest night",
+    imageLow: "/images/the_sun_in_the_brightest_night.webp",
+    imageHigh: "/images/the_sun_in_the_brightest_night.webp",
+    link: "/the_sun_in_the_brightest_night/"
+  },
+  {
     title: "Arlington",
-    image: "/images/Arlington-small.webp",
+    imageLow: "/images/Arlington-small.webp",
+    imageHigh: "/images/Arlington-small.webp"
   },
   {
     title: "The last one before bedtime",
-    image: "/images/the_last_one_before_bedtime.webp",
+    imageHigh: "/images/the_last_one_before_bedtime.webp",
+    imageLow: "/images/the_last_one_before_bedtime.webp",
     link: "/the_last_one_before_bedtime/"
   },
   {
     title: "Makulatura",
-    image: "/images/makulaturaquality=2.webp",
+    imageHigh: "/images/makulaturaquality=2.webp",
+    imageLow: "/images/makulaturaquality=2.webp",
     link: "/makulatura/"
   },
   {
+    title: "Stencil printing sampler",
+    imageHigh: "/images/stencil_printing_sampler.webp",
+    imageLow: "/images/stencil_printing_sampler.webp",
+    link: "/stencil_printing_sampler/"
+  },
+  {
     title: "White puppies",
-    image: "/images/white_puppiesquality=2.webp",
+    imageLow: "/images/white_puppiesquality=2.webp",
+    imageHigh: "/images/white_puppiesquality=2.webp",
     link: "/white_puppies/"
   }
 ];

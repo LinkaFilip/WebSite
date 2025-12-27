806618f3e17a07e09b420b32c5ac38ff1a002135
LinkaFilip <filip.linka@email.cz>
2025-12-27 20:33:17 +0100

    adding the house page and css, js, html, editing sitemap ect.

--- main/work/projects.js
+++ main/work/projects.js
@@ -2,7 +2,8 @@ const projects = [
   {
     title: "The House",
     imageLow: "/images/theHouse-small.webp",
-    imageHigh: "/images/theHouse-small.webp"
+    imageHigh: "/images/theHouse-small.webp",
+    link: "/the_house/"
   },
   {
     title: "Wrongly developed dreams or 4 harbingers of strangled sadness or when angels deserve only death",
